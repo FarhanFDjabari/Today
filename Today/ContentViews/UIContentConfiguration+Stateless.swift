604f875103eedcf628253751f66ae7e4f02e3ac0
Farhan Fadhillah Djabari <farhanf@mit.id>
2023-12-31 20:21:30 +0700

    initial commit

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Farhan Fadhilah Djabari on 25/12/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
---
+++ Today/ListViewController/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Farhan Fadhilah Djabari on 24/12/23.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}
